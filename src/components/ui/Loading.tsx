ec67774a52b5fe6bb80b320517b5121d6e88bea8
Yuki xD <pxnditxyr@gmail.com>
2022-12-31 10:23:13 -0400

    feat: applied lazy load on routes

---
+++ src/components/ui/Loading.tsx
@@ -0,0 +1,9 @@
+
+export const Loading = () => {
+  return (
+    <div>
+      <h1> Loading... </h1>
+    </div>
+  );
+
+}
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import {
   BrowserRouter,
   Routes,
@@ -6,45 +7,48 @@ import {
   Navigate,
 } from 'react-router-dom';
 
-// import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+import { Loading } from '../components';
+
+import { routes } from './routes';
 
 import logo from '../assets/yuki.svg';
-import { routes } from './routes';
 
 export const Navigation = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <nav>
-          <img src={ logo } alt="Yuki" style={{ width: '100px' }} />
-          <ul>
-            {
-              routes.map( ({ to, name }) => (
-                <li key={ to }>
-                  <NavLink
-                    to={ to }
-                    style={ ({ isActive }) => ({ color: isActive ? '#86C8BC' : '#FD8A8A' }) }
-                  > { name } </NavLink>
-                </li>
-              ))
-            }
-          </ul>
-        </nav>
+    <Suspense fallback={ <Loading /> }>
+      <BrowserRouter>
+        <div>
+          <nav>
+            <img src={ logo } alt="Yuki" style={{ width: '100px' }} />
+            <ul>
+              {
+                routes.map( ({ to, name }) => (
+                  <li key={ to }>
+                    <NavLink
+                      to={ to }
+                      style={ ({ isActive }) => ({ color: isActive ? '#86C8BC' : '#FD8A8A' }) }
+                    > { name } </NavLink>
+                  </li>
+                ))
+              }
+            </ul>
+          </nav>
 
-      </div>
-      <Routes>
-        {
-          routes.map( ({ to, path, Component }) => (
-            <Route
-              key={ to }
-              path={ path }
-              element={ <Component /> }
-              />
-          ))
-        }
+        </div>
+        <Routes>
+          {
+            routes.map( ({ to, path, Component }) => (
+              <Route
+                key={ to }
+                path={ path }
+                element={ <Component /> }
+                />
+            ))
+          }
 
-        <Route path="*" element={ <Navigate to={ routes[ 0 ].to } replace /> } />
-      </Routes>
-    </BrowserRouter>
+          <Route path="*" element={ <Navigate to={ routes[ 0 ].to } replace /> } />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 };
